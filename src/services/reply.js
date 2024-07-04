ebf923480bec54e36b67d7532eff6d35c413b57a
Marieta Harutyunyan <harutyunyanmarieta@gmail.com>
2024-07-04 15:01:07 +0400

    Sort replies by id descending

--- src/services/reply.js
+++ src/services/reply.js
@@ -3,7 +3,7 @@ const { Reply } = require("../models");
 module.exports = {
   getAll: async () => {
     try {
-      const replies = await Reply.findAll({ limit: 100 });
+      const replies = await Reply.findAll({ limit: 100, order: [ ['id', 'DESC'] ] });
       return replies;
     } catch (error) {
       console.error("Error fetching replies:", error);
